65ff5b698c2667df089d2ee52ac1b2d541cb3adc
euijeong20 <euijeong20@naver.com>
2024-12-21 16:43:50 +0900

    로비 작업중

--- Assets/Scripts/HEJ/LobyManager.cs
+++ Assets/Scripts/HEJ/LobyManager.cs
@@ -1,12 +1,24 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobyManager : MonoBehaviour
 {
+    private string[] dialogs;
+
     [SerializeField] private GameObject CreatePopup;
     [SerializeField] private GameObject FindPopup;
+    [SerializeField] private GameObject CodePopup;
+    [SerializeField] private GameObject ErrorPopup;
+    [SerializeField] private RawImage RawContent;
+
     private void Start()
     {
-        
+        dialogs = new string[] {
+            "Room ID Not Found!",
+            "This Room is Full!",
+        };
+
+
     }
     public void OpenCreatePopUP()
     {
@@ -19,5 +31,11 @@ public class LobyManager : MonoBehaviour
     {
         CreatePopup.SetActive(false);
         FindPopup.SetActive(true);
+    }
+
+    public void pickContent()
+    {
+       // RawContent.material.color = new Color(97, 118, 102);
+        Debug.Log("change");
     }
 }
