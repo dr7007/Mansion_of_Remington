af71dc5a825d0c1b96d097ebb60283f7edf5dc47
smj-95 <cprzmz@naver.com>
2025-01-16 10:02:20 +0900

    효과음 아직 다 못넣음...

--- Assets/Scripts/CDH/Real_lamp.cs
+++ Assets/Scripts/CDH/Real_lamp.cs
@@ -5,13 +5,16 @@ public class Real_lamp : MonoBehaviour
     [SerializeField]
     Animator animator;
 
+    public int onlyOne = 0;
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player" && onlyOne == 0)
         {
             Debug.Log("플레이어다");
             AudioManager.instance.PlaySfx(AudioManager.sfx.Noise);
             animator.SetBool("isEnter", true);
+            ++onlyOne;
         }
     }
 }
--- Assets/Scripts/CDH/changeArt.cs
+++ Assets/Scripts/CDH/changeArt.cs
@@ -28,4 +28,5 @@ public class changeArt : MonoBehaviour
         }
     }
 
+    
 }
